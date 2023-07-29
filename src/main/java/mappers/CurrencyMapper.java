b1e08139976e078e1e10d0837b9d4a5f6c8d2fe6
SpicyW0lf <62665736+SpicyW0lf@users.noreply.github.com>
2023-07-29 21:12:39 +0300

    feat: currToJson mapping

--- src/main/java/mappers/CurrencyMapper.java
+++ src/main/java/mappers/CurrencyMapper.java
@@ -1,11 +1,16 @@
 package mappers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import models.Currency;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CurrencyMapper {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
     public static Currency mapFromResultSet(ResultSet rs) throws SQLException {
         return new Currency(
                 rs.getInt("id"),
@@ -13,5 +18,10 @@ public class CurrencyMapper {
                 rs.getString("FullName"),
                 rs.getString("Sign")
         );
+    }
+
+    public static String mapToJson(Object curr) throws JsonProcessingException {
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return objectMapper.writeValueAsString(curr);
     }
 }
